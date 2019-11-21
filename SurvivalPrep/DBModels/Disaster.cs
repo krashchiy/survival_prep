9d712746ab3027e5e1afdc574a51e30e28622ea1
Nathan Robbins <nathancrobbins@gmail.com>
2019-11-20 21:38:53 -0700

    Created migration for item and disaster entries.

---
+++ SurvivalPrep/DBModels/Disaster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurvivalPrep.DBModels
+{
+    public class Disaster
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<ItemDisaster> ItemDisasters { get; set; }
+    }
+}
---
+++ SurvivalPrep/DBModels/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurvivalPrep.DBModels
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Cost { get; set; }
+        public ICollection<ItemDisaster> ItemDisasters { get; set; }
+    }
+}
