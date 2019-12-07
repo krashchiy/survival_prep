19fc470cec330de0f1d45aeeeaf453eb4840e654
Nathan Robbins <nathancrobbins@gmail.com>
2019-12-06 19:01:10 -0700

    Added header comments to user controller and pages.

--- SurvivalPrep/Controllers/UserController.cs
+++ SurvivalPrep/Controllers/UserController.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/**
+ * Author:    Nathan Robbins, Igor Ivanov, Jane Tian
+ * Date:      Dec 6, 2019
+ * Course:    CS 4540, University of Utah, School of Computing
+ * Copyright: CS 4540 and Nathan Robbins, Igor Ivanov, Jane Tian - This work may not be copied for use in Academic Coursework.
+ *
+ * I, Nathan Robbins, certify that I wrote this code from scratch and did not copy it in part or whole from 
+ * another source.  Any references used in the completion of the assignment are cited in my README file.
+ *
+ * File Contents
+ *
+ *    This file defines the User controller which oversees functionality relating to specific users. This includes
+ *    details pages that describe users' inventories and individual scores, as well as overall highscores pages, including
+ *    per disaster highscore pages.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,7 +38,7 @@ namespace SurvivalPrep.Controllers
 
         public IActionResult Index()
         {
-            // Redirect to High Scores page when built
+            // Redirect to High Scores page
             return RedirectToAction("Highscores");
         }
 
